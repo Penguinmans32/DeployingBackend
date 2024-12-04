1d3e1a502cc726f9cc72a480f00828cb2fcbe9f1
Penguin2 <vanessleonard.capuras@cit.edu>
2024-12-04 10:10:00 +0800

    just fixing

--- src/main/java/com/example/usersavorspace/configs/WebConfig.java
+++ src/main/java/com/example/usersavorspace/configs/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://penguinman.me")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
